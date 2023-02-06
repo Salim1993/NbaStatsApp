d699cf4fb72b81cd87c66d2396f7df278823e659
DESKTOP-POL0H0E\Salim <salimbenkhaled93@gmail.com>
2023-02-06 16:14:31 -0700

    need lirbary to make compose navigation and hilt viewmodels to work together
    viewmodel no longer complains when being used in navHost component

--- app/src/main/java/com/salim/nbastatsapp/navigation/NavigationHost.kt
+++ app/src/main/java/com/salim/nbastatsapp/navigation/NavigationHost.kt
@@ -2,11 +2,14 @@ package com.salim.nbastatsapp.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.salim.nbastatsapp.player.details.PlayerDetailScreen
+import com.salim.nbastatsapp.player.details.PlayerDetailViewModel
 import com.salim.nbastatsapp.player.list.PlayerListScreen
+import com.salim.nbastatsapp.player.list.PlayerListViewModel
 
 @Composable
 fun NavigationHost(
@@ -19,10 +22,12 @@ fun NavigationHost(
         startDestination = PlayerListNavigationInfo.PLAYER_LIST_DESTINATION_ROUTE
     ) {
         composable(route = PlayerListNavigationInfo.PLAYER_LIST_DESTINATION_ROUTE) {
-            PlayerListScreen(modifier = modifier)
+            val viewModel = hiltViewModel<PlayerListViewModel>()
+            PlayerListScreen(modifier = modifier, playerListViewModel = viewModel)
         }
         composable(PlayerDetailsNavigationInfo.PLAYER_DETAIL_DESTINATION_ROUTE) {
-            PlayerDetailScreen(modifier = modifier)
+            val viewModel = hiltViewModel<PlayerDetailViewModel>()
+            PlayerDetailScreen(modifier = modifier, playerDetailViewModel = viewModel)
         }
     }
 }
