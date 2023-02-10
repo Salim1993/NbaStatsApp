08f183466336ea6a4375ea3ab4106fae3c01ee1a
DESKTOP-POL0H0E\Salim <salimbenkhaled93@gmail.com>
2023-02-09 19:56:55 -0700

    added team list ui

--- app/src/main/java/com/salim/nbastatsapp/navigation/NavigationHost.kt
+++ app/src/main/java/com/salim/nbastatsapp/navigation/NavigationHost.kt
@@ -15,6 +15,8 @@ import com.salim.nbastatsapp.player.details.PlayerDetailScreen
 import com.salim.nbastatsapp.player.details.PlayerDetailViewModel
 import com.salim.nbastatsapp.player.list.PlayerListScreen
 import com.salim.nbastatsapp.player.list.PlayerListViewModel
+import com.salim.nbastatsapp.team.TeamListScreen
+import com.salim.nbastatsapp.team.TeamListViewModel
 
 @Composable
 fun NavigationHost(
@@ -43,6 +45,17 @@ fun NavigationHost(
         ) {
             val viewModel = hiltViewModel<PlayerDetailViewModel>()
             PlayerDetailScreen(modifier = modifier, playerDetailViewModel = viewModel)
+        }
+        composable(route = TeamsListNavigationInfo.TEAMS_LIST_DESTINATION_ROUTE) {
+            val viewModel = hiltViewModel<TeamListViewModel>()
+            TeamListScreen(
+                modifier = modifier,
+                teamListViewModel = viewModel,
+                onClickTeam = {
+                    // TODO: replace below with correct navigation
+                    //navController.navigateSingleTopTo(PlayerDetailsNavigationInfo.buildPlayerDetailRoute(it))
+                }
+            )
         }
     }
 }
--- app/src/main/java/com/salim/nbastatsapp/team/TeamsListUI.kt
+++ app/src/main/java/com/salim/nbastatsapp/team/TeamsListUI.kt
@@ -1,17 +1,15 @@
 package com.salim.nbastatsapp.team
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -20,84 +18,63 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
 import com.salim.nbastatsapp.R
-import com.salim.nbastatsapp.player.Player
-import com.salim.nbastatsapp.player.PlayerAndTeam
-import com.salim.nbastatsapp.player.list.PlayerListViewModel
 import com.salim.nbastatsapp.utilities.LogoManager
 
 @Composable
-fun PlayerListScreen(
+fun TeamListScreen(
     modifier: Modifier,
-    playerListViewModel: PlayerListViewModel = viewModel(),
-    onClickPlayer: (Int) -> Unit = {}
+    teamListViewModel: TeamListViewModel = viewModel(),
+    onClickTeam: (Int) -> Unit = {}
 ) {
-    val playerListState = playerListViewModel.playerList.collectAsState(initial = emptyList())
-    PlayerList(modifier = modifier, list = playerListState.value, onClickPlayer = onClickPlayer)
+    val teamListState = teamListViewModel.teamList.collectAsState(initial = emptyList())
+    TeamList(modifier = modifier, list = teamListState.value, onClickTeam = onClickTeam)
 }
 
 @Composable
-fun PlayerList(
+fun TeamList(
     modifier: Modifier,
-    list: List<PlayerAndTeam>,
-    onClickPlayer: (Int) -> Unit = {}
+    list: List<Team>,
+    onClickTeam: (Int) -> Unit = {}
 ) {
     LazyColumn(modifier = modifier) {
         items(list) {
-            PlayerCard(modifier = modifier, playerAndTeam = it, onClickPlayer = onClickPlayer)
+            TeamCard(modifier = modifier, team = it, onClickTeam = onClickTeam)
         }
     }
 }
 
 @Composable
-fun PlayerCard(
+fun TeamCard(
     modifier: Modifier,
-    playerAndTeam: PlayerAndTeam,
-    onClickPlayer: (Int) -> Unit = {}
+    team: Team,
+    onClickTeam: (Int) -> Unit = {}
 ) {
-    val player = playerAndTeam.player
-    val team = playerAndTeam.team
     Row(
         modifier = modifier
             .fillMaxWidth()
             .padding(8.dp)
             .clickable {
-                onClickPlayer(player.id)
+                onClickTeam(team.id)
             },
         verticalAlignment = CenterVertically
     ) {
         AsyncImage(
             modifier = modifier.heightIn(max = 48.dp),
-            model = LogoManager.getProperLogo(player.teamName),
-            contentDescription = stringResource(R.string.team_logo_content_description, player.teamName)
+            model = LogoManager.getProperLogo(team.fullName),
+            contentDescription = stringResource(R.string.team_logo_content_description, team.fullName)
         )
         Text(
             modifier = modifier.padding(horizontal = 8.dp),
-            text = "${player.firstName} ${player.lastName}"
+            text = team.fullName
         )
-        Box(modifier = Modifier.weight(1.0f)) {
-            Text(
-                modifier = modifier.align(Alignment.CenterEnd),
-                text = team?.fullName ?: stringResource(id = R.string.not_on_team)
-            )
-        }
     }
 }
 
 @Preview
 @Composable
 fun PreviewPlayerCard() {
-    val player = Player(
-        id = 0,
-        firstName = "LMAO",
-        heightFeet = null,
-        heightInches = null,
-        lastName = "ROFL",
-        position = "",
-        teamName = "",
-        weightPounds = null
-    )
     val team = Team(
-        id = 0, abbreviation = "", city = "", conference = "", division = "", fullName = "L TEAM", name = ""
+        id = 0, abbreviation = "", city = "", conference = "", division = "", fullName = "Golden State Warrior", name = ""
     )
-    PlayerCard(modifier = Modifier, playerAndTeam = PlayerAndTeam(player, team))
+    TeamCard(modifier = Modifier, team = team)
 }
