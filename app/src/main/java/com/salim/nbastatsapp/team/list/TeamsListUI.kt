8ce137e2a86946044e160721bd1917127bb21d35
DESKTOP-POL0H0E\Salim <salimbenkhaled93@gmail.com>
2023-05-03 14:13:46 -0600

    added new team list UI, shows them all in grid with logos

--- app/src/main/java/com/salim/nbastatsapp/team/list/TeamsListUI.kt
+++ app/src/main/java/com/salim/nbastatsapp/team/list/TeamsListUI.kt
@@ -1,20 +1,24 @@
 package com.salim.nbastatsapp.team.list
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material3.Divider
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Alignment.Companion.CenterVertically
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -39,7 +43,10 @@ fun TeamList(
     list: List<Team>,
     onClickTeam: (Int) -> Unit = {}
 ) {
-    LazyColumn(modifier = modifier) {
+    LazyVerticalGrid(
+        modifier = modifier,
+        columns = GridCells.Adaptive(minSize = 128.dp)
+    ) {
         items(list) {
             TeamCard(modifier = modifier, team = it, onClickTeam = onClickTeam)
         }
@@ -52,32 +59,63 @@ fun TeamCard(
     team: Team,
     onClickTeam: (Int) -> Unit = {}
 ) {
-    Row(
+    val backGroundColor = LogoManager.getLogoColor(team.fullName)
+    Column(
         modifier = modifier
             .fillMaxWidth()
             .padding(8.dp)
+            .background(color = backGroundColor, shape = RoundedCornerShape(20.dp))
             .clickable {
                 onClickTeam(team.id)
-            },
-        verticalAlignment = CenterVertically
+            }
+            .clip(shape = RoundedCornerShape(5.dp))
+            .defaultMinSize(minHeight = 165.dp),
+        horizontalAlignment = CenterHorizontally
     ) {
         AsyncImage(
-            modifier = modifier.heightIn(max = 48.dp),
+            modifier = modifier.heightIn(max = 96.dp),
             model = LogoManager.getProperLogo(team.fullName),
             contentDescription = stringResource(R.string.team_logo_content_description, team.fullName)
         )
+        val textColor = LogoManager.getTextColor(team.fullName)
         Text(
-            modifier = modifier.padding(horizontal = 8.dp),
-            text = team.fullName
+            modifier = modifier
+                .padding(horizontal = 4.dp),
+            color = textColor,
+            text = team.fullName,
+            textAlign = TextAlign.Center
         )
     }
 }
 
 @Preview
 @Composable
-fun PreviewPlayerCard() {
+fun PreviewTeamCard() {
     val team = Team(
-        id = 0, abbreviation = "", city = "", conference = "", division = "", fullName = "Golden State Warrior", name = ""
+        id = 0,
+        abbreviation = "",
+        city = "",
+        conference = "",
+        division = "",
+        fullName = "Golden State Warriors",
+        name = ""
     )
     TeamCard(modifier = Modifier, team = team)
+}
+
+@Preview
+@Composable
+fun PreviewTeamList() {
+    val team = Team(
+        id = 0,
+        abbreviation = "",
+        city = "",
+        conference = "",
+        division = "",
+        fullName = "Golden State Warriors",
+        name = ""
+    )
+    val team2 = team.copy(fullName = "Philadelphia 76ers")
+    val list = listOf(team, team2)
+    TeamList(modifier = Modifier, list = list)
 }
--- app/src/main/java/com/salim/nbastatsapp/utilities/LogoManager.kt
+++ app/src/main/java/com/salim/nbastatsapp/utilities/LogoManager.kt
@@ -1,6 +1,8 @@
 package com.salim.nbastatsapp.utilities
 
+import androidx.compose.ui.graphics.Color
 import com.salim.nbastatsapp.R
+import com.salim.nbastatsapp.ui.theme.NbaTeamColors
 
 object LogoManager {
 
@@ -48,6 +50,80 @@ object LogoManager {
             "Utah Jazz" -> R.drawable.utah_jazz_logo
             "Washington Wizards" -> R.drawable.washington_wizards_logo
             else -> R.drawable.team_not_available
+        }
+    }
+
+    @Suppress("CyclomaticComplexMethod")
+    fun getLogoColor(teamName: String): Color {
+        return when (teamName) {
+            "Atlanta Hawks" -> NbaTeamColors.AtlantaHawks
+            "Boston Celtics" -> NbaTeamColors.BostonCeltics
+            "Brooklyn Nets" -> NbaTeamColors.BrooklynNets
+            "Charlotte Hornets" -> NbaTeamColors.CharlotteHornets
+            "Chicago Bulls" -> NbaTeamColors.ChicagoBulls
+            "Cleveland Cavaliers" -> NbaTeamColors.ClevelandCavaliers
+            "Dallas Mavericks" -> NbaTeamColors.DallasMavericks
+            "Denver Nuggets" -> NbaTeamColors.DenverNuggets
+            "Detroit Pistons" -> NbaTeamColors.DetroitPistons
+            "Golden State Warriors" -> NbaTeamColors.GoldenStateWarriors
+            "Houston Rockets" -> NbaTeamColors.HoustonRockets
+            "Indiana Pacers" -> NbaTeamColors.IndianaPacers
+            "LA Clippers" -> NbaTeamColors.LAClippers
+            "Los Angeles Lakers" -> NbaTeamColors.LosAngelesLakers
+            "Memphis Grizzlies" -> NbaTeamColors.MemphisGrizzlies
+            "Miami Heat" -> NbaTeamColors.MiamiHeat
+            "Milwaukee Bucks" -> NbaTeamColors.MilwaukeeBucks
+            "Minnesota Timberwolves" -> NbaTeamColors.MinnesotaTimberwolves
+            "New Orleans Pelicans" -> NbaTeamColors.NewOrleansPelicans
+            "New York Knicks" -> NbaTeamColors.NewYorkKnicks
+            "Oklahoma City Thunder" -> NbaTeamColors.OklahomaCityThunder
+            "Orlando Magic" -> NbaTeamColors.OrlandoMagic
+            "Philadelphia 76ers" -> NbaTeamColors.Philadelphia76ers
+            "Phoenix Suns" -> NbaTeamColors.PhoenixSuns
+            "Portland Trail Blazers" -> NbaTeamColors.PortlandTrailBlazers
+            "Sacramento Kings" -> NbaTeamColors.SacramentoKings
+            "San Antonio Spurs" -> NbaTeamColors.SanAntonioSpurs
+            "Toronto Raptors" -> NbaTeamColors.TorontoRaptors
+            "Utah Jazz" -> NbaTeamColors.UtahJazz
+            "Washington Wizards" -> NbaTeamColors.WashingtonWizards
+            else -> Color.Black
+        }
+    }
+
+    @Suppress("CyclomaticComplexMethod")
+    fun getTextColor(teamName: String): Color {
+        return when (teamName) {
+            "Atlanta Hawks" -> Color.White
+            "Boston Celtics" -> Color.White
+            "Brooklyn Nets" -> Color.White
+            "Charlotte Hornets" -> Color.White
+            "Chicago Bulls" -> Color.White
+            "Cleveland Cavaliers" -> Color.White
+            "Dallas Mavericks" ->Color.White
+            "Denver Nuggets" -> Color.White
+            "Detroit Pistons" -> Color.White
+            "Golden State Warriors" -> Color.White
+            "Houston Rockets" -> Color.White
+            "Indiana Pacers" -> Color.White
+            "LA Clippers" -> Color.White
+            "Los Angeles Lakers" -> Color.Black
+            "Memphis Grizzlies" -> Color.White
+            "Miami Heat" -> Color.White
+            "Milwaukee Bucks" -> Color.White
+            "Minnesota Timberwolves" -> Color.White
+            "New Orleans Pelicans" -> Color.White
+            "New York Knicks" -> Color.White
+            "Oklahoma City Thunder" -> Color.White
+            "Orlando Magic" -> Color.White
+            "Philadelphia 76ers" -> Color.White
+            "Phoenix Suns" -> Color.White
+            "Portland Trail Blazers" -> Color.White
+            "Sacramento Kings" -> Color.White
+            "San Antonio Spurs" -> Color.Black
+            "Toronto Raptors" -> Color.White
+            "Utah Jazz" -> Color.White
+            "Washington Wizards" -> Color.White
+            else -> Color.Black
         }
     }
 }
